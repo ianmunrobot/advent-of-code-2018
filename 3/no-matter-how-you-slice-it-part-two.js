b8c89f0f8ea808a59c20d362b2996a7a61d7aeb3
Ian Munro <ianmunrobot@gmail.com>
2018-12-03 09:53:31 -0500

    lazy day three solutions

---
+++ 3/no-matter-how-you-slice-it-part-two.js
@@ -0,0 +1,56 @@
+const { readFile } = require('fs');
+
+readFile('input.txt', 'utf-8', (err, dataBuffer) => {
+  if (err) throw err;
+
+  const fabric = {};
+
+  const input = dataBuffer
+    .toString()
+    .split('\n');
+
+  let total = 0;
+
+  input.forEach(claim => {
+    const captures = claim.match(/#(\d*)\s@\s(\d*),(\d*):\s(\d*)x(\d*)/).map(Number);
+    const [_, id, startLeft, startTop, width, height] = captures;
+
+    for (let y = startTop; y < startTop + height; y++) {
+      if (!fabric[y]) {
+        fabric[y] = {};
+      }
+      for (let x = startLeft; x < startLeft + width; x++) {
+        if (fabric[y][x] === 1) {
+          total++;
+          fabric[y][x] = 'counted'
+        } else if (fabric[y][x] === 'counted'){
+          continue;
+        } else {
+          fabric[y][x] = 1;
+        }
+
+      }
+    }
+  });
+
+  // V lazy solution
+  let winner = null;
+  input.forEach(claim => {
+    const captures = claim.match(/#(\d*)\s@\s(\d*),(\d*):\s(\d*)x(\d*)/).map(Number);
+    const [_, id, startLeft, startTop, width, height] = captures;
+
+    let dirty = false;
+    for (let y = startTop; y < startTop + height; y++) {
+      for (let x = startLeft; x < startLeft + width; x++) {
+        if (fabric[y][x] === 'counted'){
+          dirty = true;
+          break;
+        }
+      }
+    }
+    if (!dirty) {
+      winner = id;
+    }
+  });
+  console.log(winner);
+});
